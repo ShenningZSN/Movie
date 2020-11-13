8345c23944dbc577cccba4cffad9c73232b9d2da
Yun Xie <xieyun95@outlook.com>
2020-11-12 22:42:22 -0800

    Nov12: try to add image

--- searching.js
+++ searching.js
@@ -19,12 +19,21 @@ function findMovieByKeyword(keyword, page) {
 		data: { "api_key": "21c2f2edc4b87ed7ca1bab78ecee5012" },
 		dataType: "json",
 		success: function (result, status, xhr) {
-			console.log(result["results"].length);
+			console.log(result["results"][0]);
 			var resultHTML = $("#searchResult");
 			resultHTML.empty();
+
 			for (let i = 0; i < result["results"].length; i++) {
-				resultHTML.append("<li>" + result["results"][i]["title"] + "</li>");
+				var movieImage = "https://image.tmdb.org/t/p/w500/" + result["results"][i]["poster_path"];
+				var movieTitle = result["results"][i]["title"];
+				
+				resultHTML.append(
+				"<div class=\"movie\">" + 
+					"<p>" + movieTitle + "</p>" + 
+				    "<img src=\"" + movieImage + "\" >" + 
+				"</div>");
 			}
+			
 		},
 		error: function (xhr, status, error) {
 			$("#message").html("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
