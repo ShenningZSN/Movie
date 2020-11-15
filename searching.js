fc906dc9662ec2d2dcd0979feb54a7cf4c62c9d5
Yun Xie <xieyun95@outlook.com>
2020-11-15 00:51:54 -0800

    handle invalid input and lost image

--- searching.js
+++ searching.js
@@ -4,8 +4,12 @@ $(document).ready(
 
 function search() {
 	$("#searchButton").click(function () {
-        var input = document.getElementById("target").value;
-        findMovieByKeyword(input, 1);
+		var keyword_input = document.getElementById("target").value;
+		if (keyword_input == "" || keyword_input == null) {
+			alert("Please enter a valid keyword!");
+			return;
+		}
+        findMovieByKeyword(keyword_input, 1);
     });
 }
 
@@ -19,21 +23,26 @@ function findMovieByKeyword(keyword, page) {
 		data: { "api_key": "21c2f2edc4b87ed7ca1bab78ecee5012" },
 		dataType: "json",
 		success: function (result, status, xhr) {
-			console.log(result["results"][0]);
+			
 			var resultHTML = $("#searchResult");
 			resultHTML.empty();
 
 			for (let i = 0; i < result["results"].length; i++) {
-				var movieImage = "https://image.tmdb.org/t/p/w500/" + result["results"][i]["poster_path"];
+				console.log(result["results"][i]);
+				var movieImage = result["results"][i]["poster_path"] === null ? 
+								"failed-Image.png" : 
+								"https://image.tmdb.org/t/p/w500/" + 
+				                 result["results"][i]["poster_path"];
+				
 				var movieTitle = result["results"][i]["title"];
 				var movieDescription = result["results"][i]["overview"];
 				
 
 				let block = document.createElement("div");
-				block.className = "clearfix";
+				block.className = "movie-block clearfix";
 			
 				let poster = document.createElement("img");
-				poster.className = "float-left";
+				poster.className = "movie-poster float-left";
 				poster.setAttribute("src", movieImage);
 
 				let title = document.createElement("h3");
